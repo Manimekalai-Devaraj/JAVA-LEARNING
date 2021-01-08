2af4399b6105c7c462653f4085d9aecff39dce9a
Manimekalai-Devaraj <75720171+Manimekalai-Devaraj@users.noreply.github.com>
2021-01-08 13:27:14 +0530

    Add files via upload

--- java_programs/StringAndStringBuffer/StringConstantPool.java
+++ java_programs/StringAndStringBuffer/StringConstantPool.java
@@ -1,20 +1,18 @@
 public class StringConstantPool {
     public static void main(String[] args){
         // This file covers SCP - String Constant Pool
-        // In the heap memory SCP same instance will be used for these variables
+        // SCP is in the heap memory - If values are same the same instance will be used for these two variables
         String TestString1 = "Mani";
         String TestString2 = "Mani";
 
         // Note that this == compares whether 
         // TestString1 and TestString2 refer to same object or not 
+        // Result will be Equal
         if(TestString1 == TestString2){
             System.out.println("Equal");
         }else{
             System.out.println("Not equal");
         }
-        // For the above case use equalsIgnoreCase() or compareTo()
-        System.out.println("String compareTo() "+TestString1.compareTo(TestString2)); //0 because both are equal  
-
 
         System.out.println("---- Using New keyword ----");
         // new key word used so new memory will be allocated in heap but outside SCP
@@ -22,6 +20,7 @@ public class StringConstantPool {
 
         // Note that this == compares whether 
         // TestString1 and TestString3 refer to same object or not 
+        // Result will be Not equal
         if(TestString1 == TestString3){
             System.out.println("Equal");
         }else{
